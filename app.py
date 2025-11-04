7a13e94deaabe7bd909ed355da0b3d0422029c2e
Jacob Stoffel <jacob-t-stoffel@uiowa.edu>
2025-11-03 19:21:35 -0600

    Bootstrap Setup
    added bootstrap setup to the base.html

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-# BAIS:3300 — Digital Product Management
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    # Step 1: return text (Hello World!!) for first screenshot
-    return "Hello World!!"
+    return render_template("index.html")
+
+@app.route("/about")
+def about_page():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002, debug=True)
+
